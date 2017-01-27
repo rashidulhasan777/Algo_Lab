ba22411c98e9da295ae771228d69b102f4b4f628
Md Rashidul Hasan <rashidulhasan777@gmail.com>
2017-01-27 14:15:46 +0600

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -10,19 +10,20 @@ int main()
         cin >> ara[i];
     for(int i=0;i<n;i++)
     {
-        maximum=ara[0];
         index=0;
         for(int j=1;j<n-i;j++)
         {
-            if(maximum<ara[j])
+            if(ara[index]<ara[j])
             {
-                maximum=ara[j];
                 index=j;
             }
         }
-        temp=ara[n-i-1];
-        ara[n-i-1]=ara[index];
-        ara[index]=temp;
+        if(index!=0)
+        {
+            temp=ara[n-i-1];
+            ara[n-i-1]=ara[index];
+            ara[index]=temp;
+        }
     }
     for(int i=0;i<n;i++)
         cout << ara[i] << "\t";
